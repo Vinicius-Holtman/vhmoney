f44724406235503afcbd325eec5aa05429166022
vinicius <viniholtman28@gmail.com>
2022-03-26 17:49:08 -0300

    Created components header and dashboard

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
+import { Dashboard } from "./components/Dashboard/intex";
+import { Header } from "./components/Header";
 import { GlobalStyles } from "./styles/global";
 
 export function App() {
   return (
-    <div className="App">
-      <h1>Hello Word</h1>
+    <>
+      <Header />
+      <Dashboard />
       <GlobalStyles />
-    </div>
+    </>
   );
 }
