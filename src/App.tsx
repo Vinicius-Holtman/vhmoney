e5aa12cb62e66feae23d9c526652a2a9a9ad1f7c
vinicius <viniholtman28@gmail.com>
2022-03-26 23:19:02 -0300

    Created modal for create new transaction

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,31 @@
+import { useState } from 'react';
+import Modal from 'react-modal'
+
 import { Dashboard } from "./components/Dashboard/intex";
 import { Header } from "./components/Header";
 import { GlobalStyles } from "./styles/global";
 
 export function App() {
+  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false);
+
+  function handleOpenNewTransactionModal() {
+    setIsNewTransactionModalOpen(true)
+  }
+
+  function handleCloseNewTransactionModal() {
+    setIsNewTransactionModalOpen(false)
+  }
+
   return (
     <>
-      <Header />
+      <Header onOpenNewTransactionModal={handleOpenNewTransactionModal}/>
+
       <Dashboard />
+
+      <Modal isOpen={isNewTransactionModalOpen} onRequestClose={handleCloseNewTransactionModal}>
+        <h2>Cadastrar Informacao</h2>
+      </Modal>
+      
       <GlobalStyles />
     </>
   );
